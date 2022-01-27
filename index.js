189dc44d55371829358d134fbd82ca7fa82eb5b0
Diego Barreto <diegobarreto5000@gmail.com>
2022-01-27 01:36:03 -0500

    Starting a function

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
-/*
+
 for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 };
-*/
-for(let i = 0; i < monthlyExpenseQuestion.length; i++){
-    let answer1 = parseFloat(window.prompt(monthlyExpenseQuestion[i]))
-        monthlyExpenses = monthlyExpenses + answer1;
-}
 
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+};
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+};
