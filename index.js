24ce528c9928fb4f738f607fd77334a4a655d8fb
Diego Barreto <diegobarreto5000@gmail.com>
2022-01-26 21:32:07 -0500

    Serching for errors

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
-
-for(let i =0; i<weeklyExpenseQuestions.length; i++){
+/*
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
-        weeklyExpenses = weeklyExpenses + answer;
+    weeklyExpenses = weeklyExpenses + answer
+};
+*/
+for(let i = 0; i < monthlyExpenseQuestion.length; i++){
+    let answer1 = parseFloat(window.prompt(monthlyExpenseQuestion[i]))
+        monthlyExpenses = monthlyExpenses + answer1;
 }
 
-for(let i =0; i<monthlyExpensesQuestions.length; i++){
-    let answer = parseFloat(window.prompt(monthlyExpensesQuestions[i]))
-        monthlyExpenses = monthlyExpenses + answer;
-}
-
-for(let i =0; i<annualExpenseQuestions.length; i++){
-     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
-        annualExpenses = annualExpenses + answer;
-}
